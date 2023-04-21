23338ca544ee1f18fa8b8a677c78e02a16ef000c
Deblin798 <deblin.chaudhuri@gmail.com>
2023-04-21 12:57:58 +0530

    Sidebar implementation

--- src/contexts/ContextProvide.js
+++ src/contexts/ContextProvide.js
@@ -2,22 +2,33 @@ import React, {createContext, useContext, useState} from "react";
 
 const StateContext = createContext();
 
-const inititalState = {
+const initialState = {
     chat: false,
     cart: false,
-    userProfile: false,
-    notification: false
+    notification: false,
+    userProfile: false
 }
 
 export const ContextProvider = ({ children }) => {
 
     const [activeMenu, setActiveMenu] = useState(true)
+    const [isClicked, setIsClicked] = useState(initialState)
+    const [screenSize, setScreenSize] = useState(undefined)
+
+    const handleClick = (clicked) => {
+        setIsClicked({...initialState, [clicked]: true})
+    }
 
     return (
         <StateContext.Provider
             value = {{ 
-                activeMenu: activeMenu,
-                setActiveMenu: setActiveMenu,
+                activeMenu,
+                setActiveMenu,
+                isClicked,
+                setIsClicked,
+                handleClick,
+                screenSize,
+                setScreenSize
             }}>
             {children}
         </StateContext.Provider>
